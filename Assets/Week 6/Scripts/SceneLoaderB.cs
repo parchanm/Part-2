dab0dd336384065b9b035b50fc6de1bd5e9ef89a
Chanmin <parchanm@sheridancollege.ca>
2024-02-14 10:48:57 -0500

    Task2 completed: Made scene B load week 5 scene with keydown space

---
+++ Assets/Week 6/Scripts/SceneLoaderB.cs
@@ -0,0 +1,19 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderB : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
